4fbee9b3ea1b98df26499be5191581af39c85b9d
Taylor Brown <taybrown9000@gmail.com>
2020-11-03 13:38:58 -0800

    first commit

---
+++ library.js
@@ -0,0 +1,10 @@
+function Book(title, author, pages, hasBeenRead) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.hasBeenRead = hasBeenRead;
+
+	this.info = function() {
+		return this.title;
+	}
+}
